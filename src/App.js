ef3bc8648aea440bd72b6e53cf6628b25b7eece7
aanchal <waliaaanchal02@gmail.com>
2022-03-12 19:44:40 +0530

    up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Card, Row } from 'react-bootstrap';
+
+
+
+
+
 function App() {
   const [quote, setQuote] = useState('');
   const getQuote = () => {
@@ -19,7 +24,7 @@ function App() {
     getQuote();
   }, {});
   return (
-    <div className="App" id="quote-box">
+    <div className="App h-100" id="quote-box" >
       <nav class="navbar navbar-dark bg-dark  ">
   <i class="navbar-brand mx-auto h1">Quotes.</i>
 </nav>
@@ -28,15 +33,21 @@ function App() {
   <div class="card-header">
     Quote
   </div>
-  <div class="card-body">
+  <div class="card-body"
+  
+  
+  
+  >
     <blockquote class="blockquote mb-0">
       <p>{quote.text}</p>
       <footer class="blockquote-footer">{quote.author}</footer>
     </blockquote>
   </div>
 </div>
-        <div className="d-flex align-items-center w-100 justify-content-around my-5 "><button className="btn btn-warning  mx-5" onClick={getQuote} id="new-quote"> Get Quote.</button>
-        
+        <div className="d-flex align-items-center w-25 justify-content-around my-5 ">
+          
+          <button className="btn btn-warning mr-5 " onClick={getQuote} id="new-quote"> Get Quote.</button>
+            
           
           <button className="btn btn-primary"  onClick={() => { window.location.replace("https://twitter.com/intent/tweet")}}>   Tweet   </button>
           
